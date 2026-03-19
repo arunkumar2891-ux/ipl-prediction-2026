6af8dd7107124f1bfa2b9d84ad7dac1c1d7fe92c
arunkumar2891-ux <arunkumar2891@gmail.com>
2026-03-19 12:51:48 +0530

    Add files via upload

---
+++ frontend/src/data/members.ts
@@ -0,0 +1,206 @@
+export const members = [
+	{
+		"Email": "arjunsuresh2104@gmail.com",
+		"Name": "Arjun",
+		"Group": "G1",
+		"Amount": 0
+	},
+	{
+		"Email": "iamarunkumor@gmail.com",
+		"Name": "Arunkumar J S",
+		"Group": "G1",
+		"Amount": 0
+	},
+	{
+		"Email": "adhivm@gmail.com",
+		"Name": "Adhi vm",
+		"Group": "G1",
+		"Amount": 0
+	},
+	{
+		"Email": "rbala045@gmail.com",
+		"Name": "Balaji",
+		"Group": "G1",
+		"Amount": 0
+	},
+	{
+		"Email": "charanmohan05@gmail.com",
+		"Name": "Charan Mohan R",
+		"Group": "G1",
+		"Amount": 0
+	},
+	{
+		"Email": "gopi13karthick@gmail.com",
+		"Name": "Gopinath K",
+		"Group": "G1",
+		"Amount": 0
+	},
+	{
+		"Email": "bgs.shankar@gmail.com",
+		"Name": "Gowri Shankar",
+		"Group": "G1",
+		"Amount": 0
+	},
+	{
+		"Email": "nw.gowtham@gmail.com",
+		"Name": "Gowtham Tamilselvan",
+		"Group": "G1",
+		"Amount": 0
+	},
+	{
+		"Email": "harisriram.h14@gmail.com",
+		"Name": "Hariharan",
+		"Group": "G1",
+		"Amount": 0
+	},
+	{
+		"Email": "sriniha2.dec06@gmail.com",
+		"Name": "Hariprasad Srinivasan",
+		"Group": "G1",
+		"Amount": 0
+	},
+	{
+		"Email": "kishorezum07@gmail.com",
+		"Name": "Kishore",
+		"Group": "G1",
+		"Amount": 0
+	},
+	{
+		"Email": "midun.mib@gmail.com",
+		"Name": "Midun",
+		"Group": "G1",
+		"Amount": 0
+	},
+	{
+		"Email": "naveenalexisit@gmail.com",
+		"Name": "Naveen",
+		"Group": "G1",
+		"Amount": 0
+	},
+	{
+		"Email": "prabhud465@gmail.com",
+		"Name": "Prabhu",
+		"Group": "G1",
+		"Amount": 0
+	},
+	{
+		"Email": "sathya.it39@gmail.com",
+		"Name": "Sathya",
+		"Group": "G1",
+		"Amount": 0
+	},
+	{
+		"Email": "sethusandhiya44@gmail.com",
+		"Name": "Sethupathy",
+		"Group": "G1",
+		"Amount": 0
+	},
+	{
+		"Email": "sri.true@gmail.com",
+		"Name": "Srivatsan",
+		"Group": "G1",
+		"Amount": 0
+	},
+	{
+		"Email": "sundarster@gmail.com",
+		"Name": "Sundar",
+		"Group": "G1",
+		"Amount": 0
+	},
+	{
+		"Email": "venkat1979gc@gmail.com",
+		"Name": "Venkat",
+		"Group": "G1",
+		"Amount": 0
+	},
+	{
+		"Email": "isudarsan93@gmail.com",
+		"Name": "Susa",
+		"Group": "G1",
+		"Amount": 0
+	},
+	{
+		"Email": "vinay.baskie@gmail.com",
+		"Name": "Vinay",
+		"Group": "G1",
+		"Amount": 0
+	},
+	{
+		"Email": "vijay.raghavan4321@gmail.com",
+		"Name": "Vijay Raghavan",
+		"Group": "G1",
+		"Amount": 0
+	},
+	{
+		"Email": "parthiece08@gmail.com",
+		"Name": "Parthi",
+		"Group": "G1",
+		"Amount": 0
+	},
+	{
+		"Email": "parthiece08@gmail.com",
+		"Name": "Parthi",
+		"Group": "G2",
+		"Amount": 0
+	},
+	{
+		"Email": "singh.ajaykumar@gmail.com",
+		"Name": "Ajay kumar Singh",
+		"Group": "G2",
+		"Amount": 0
+	},
+	{
+		"Email": "ashsubram@gmail.com",
+		"Name": "Ashok",
+		"Group": "G2",
+		"Amount": 0
+	},
+	{
+		"Email": "lord.deeps@gmail.com",
+		"Name": "Deepak Alakkal",
+		"Group": "G2",
+		"Amount": 0
+	},
+	{
+		"Email": "mailjisjoseph@gmail.com",
+		"Name": "Jis",
+		"Group": "G2",
+		"Amount": 0
+	},
+	{
+		"Email": "matrixmohanbabu@gmail.com",
+		"Name": "Mohanbabu",
+		"Group": "G2",
+		"Amount": 0
+	},
+	{
+		"Email": "skaran1988@gmail.com",
+		"Name": "Praba",
+		"Group": "G2",
+		"Amount": 0
+	},
+	{
+		"Email": "prash.cts@gmail.com",
+		"Name": "Prashanth",
+		"Group": "G2",
+		"Amount": 0
+	},
+	{
+		"Email": "ramya.cse2@gmail.com",
+		"Name": "Ramya Ganesh",
+		"Group": "G2",
+		"Amount": 0
+	},
+	{
+		"Email": "srinivaskumar.mv30@gmail.com",
+		"Name": "Srinivas Kumar",
+		"Group": "G2",
+		"Amount": 0
+	},
+	{
+		"Email": "vedantvyas91@gmail.com",
+		"Name": "Vedant",
+		"Group": "G2",
+		"Amount": 0
+	}
+];
---
+++ frontend/src/types/env.d.ts
@@ -0,0 +1,7 @@
+interface Window {
+  env: {
+    API_TOKEN_DEV: string;
+    LEADER_TOKEN_DEV: string;
+    BID_TOKEN_DEV: string;
+  };
+}
