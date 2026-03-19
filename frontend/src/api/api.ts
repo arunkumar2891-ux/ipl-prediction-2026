b0e441b5787fe55144258c0e75e3854b50b15531
arunkumar2891-ux <arunkumar2891@gmail.com>
2026-03-19 14:21:13 +0530

    Update api.ts

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -1,9 +1,11 @@
+const API_URL = import.meta.env.VITE_API_URL;
+console.log(import.meta.env.VITE_API_URL);
+console.log(`API_URL: ${API_URL}`);
+
 export const api = {
 
  submitPrediction: async (payload) => {
-   const API_URL = import.meta.env.VITE_API_URL;
-   console.log(import.meta.env.VITE_API_URL);
-   console.log(`API_URL: ${API_URL}`);
+   
    const res = await fetch(`${API_URL}/api/prediction`, {
      method: "POST",
      headers: { "Content-Type": "application/json" },
